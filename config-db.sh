9189f015e3952420d88a5264cdb3c1c11e721d5c
ahkarminmyat <dev.ahkarminmyat98@gmail.com>
2024-04-15 18:49:22 +0700

    restore db

---
+++ config-db.sh
@@ -0,0 +1,22 @@
+## load sample database
+sudo apt-get install curl
+
+curl -O https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
+
+unzip dvdrental.zip
+
+sudo -u postgres psql
+
+CREATE DATABASE dvdrental;
+
+## restore database 
+## before restore database, open connection 
+
+sudo vim udo vim /etc/postgresql/16/main/pg_hba.conf
+# Database administrative login by Unix domain socket
+local   all             postgres                               md5
+
+sudo systemctl restart postgrel
+
+## RESTROE DVDRENTAL
+pg_restore -U postgres --dbname=dvdrental --verbose dvdrental.tar
